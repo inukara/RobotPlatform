6b836905a85560b023a99d2b49ac48273106c4da
inukara <17498820+inukara@users.noreply.github.com>
2023-11-06 15:59:22 +0900

    drive_test_client: fix typo

--- drive_test_client.py
+++ drive_test_client.py
@@ -9,7 +9,7 @@ class DriveTestClient:
 
     def manual_drive_request(self, direction, speed):
         assert direction in ['forward', 'backward', 'left', 'right', 'turn_cw', 'turn_ccw', 'stop']
-        assert 0 <= speed <= 1
+        assert 0 <= float(speed) <= 1
         req_data = {'action': direction, 'speed': speed}
         try:
             r = requests.post(
@@ -28,20 +28,20 @@ if __name__ == '__main__':
     while True:
         key = input('Enter a command: ')
         if key == 'w':
-            print(client.manual_drive_reqeust('forward', speed))
+            print(client.manual_drive_request('forward', speed))
         elif key == 's':
-            print(client.manual_drive_reqeust('backward', speed))
+            print(client.manual_drive_request('backward', speed))
         elif key == 'a':
-            print(client.manual_drive_reqeust('left', speed))
+            print(client.manual_drive_request('left', speed))
         elif key == 'd':
-            print(client.manual_drive_reqeust('right', speed))
+            print(client.manual_drive_request('right', speed))
         elif key == 'q':
-            print(client.manual_drive_reqeust('turn_ccw', speed))
+            print(client.manual_drive_request('turn_ccw', speed))
         elif key == 'e':
-            print(client.manual_drive_reqeust('turn_cw', speed))
+            print(client.manual_drive_request('turn_cw', speed))
         elif key == 't':
             speed = input("Enter speed: ")
         elif key == 'x':
-            print(client.manual_drive_reqeust('stop', speed))
+            print(client.manual_drive_request('stop', speed))
         else:
             print('invalid key')
