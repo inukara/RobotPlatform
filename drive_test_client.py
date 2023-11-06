c04e66722fe30df4f75d7041028200bcd8fe2d27
inukara <17498820+inukara@users.noreply.github.com>
2023-11-06 13:02:40 +0900

    drive_test_client: add speed setting

--- drive_test_client.py
+++ drive_test_client.py
@@ -7,10 +7,15 @@ class DriveTestClient:
         self.url = 'http://192.168.0.3:1881'
         self.headers = {'Content-Type': 'application/json'}
 
-    def request(self, action):
-        req_data = {'action': action}
+    def manual_drive_request(self, direction, speed):
+        assert direction in ['forward', 'backward', 'left', 'right', 'turn_cw', 'turn_ccw', 'stop']
+        assert 0 <= speed <= 1
+        req_data = {'action': direction, 'speed': speed}
         try:
-            r = requests.post(self.url + '/drive', json=json.dumps(req_data), headers=self.headers)
+            r = requests.post(
+                self.url + '/drive',
+                json=json.dumps(req_data),
+                headers=self.headers)
             return json.loads(r.text)
         except Exception as e:
             print(e)
@@ -19,21 +24,24 @@ class DriveTestClient:
 
 if __name__ == '__main__':
     client = DriveTestClient()
+    speed = 0.1
     while True:
         key = input('Enter a command: ')
         if key == 'w':
-            print(client.request('forward'))
+            print(client.manual_drive_reqeust('forward', speed))
         elif key == 's':
-            print(client.request('backward'))
+            print(client.manual_drive_reqeust('backward', speed))
         elif key == 'a':
-            print(client.request('left'))
+            print(client.manual_drive_reqeust('left', speed))
         elif key == 'd':
-            print(client.request('right'))
+            print(client.manual_drive_reqeust('right', speed))
         elif key == 'q':
-            print(client.request('turn_ccw'))
+            print(client.manual_drive_reqeust('turn_ccw', speed))
         elif key == 'e':
-            print(client.request('turn_cw'))
+            print(client.manual_drive_reqeust('turn_cw', speed))
+        elif key == 't':
+            speed = input("Enter speed: ")
         elif key == 'x':
-            print(client.request('stop'))
+            print(client.manual_drive_reqeust('stop', speed))
         else:
             print('invalid key')
