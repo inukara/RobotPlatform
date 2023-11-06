a118741b4745298c439996f1ae9084c4fd0f6ec7
inukara <17498820+inukara@users.noreply.github.com>
2023-11-06 12:02:41 +0900

    drive: code cleanup

--- drive.py
+++ drive.py
@@ -1,5 +1,6 @@
 import time
 import busio
+from typing import List
 
 from board import SCL, SDA
 from adafruit_pca9685 import PCA9685
@@ -7,96 +8,50 @@ from adafruit_motor import motor
 
 class Drive:
     def __init__(self):
-        i2c = busio.I2C(SCL, SDA)
-        pca = PCA9685(i2c, address=0x40)
-        pca.frequency = 100
-        self.delay = 1
+        self.MOTOR_SAFE_DELAY = 0.01
         self.motor_speed = 0.1
-        duty_channels = [0, 3, 6, 9]
-        for i in duty_channels:
-            pca.channels[i].duty_cycle = 0xFFFF
 
+        self.STOP = 0
+        self.FORWARD = 1
+        self.BACKWARD = 2
+        self.TURN_CW = 3
+        self.TURN_CCW = 4
+        self.LEFT = 5
+        self.RIGHT = 6
+        
+        self.speeds = [
+            [0, 0, 0, 0], # STOP
+            [-self.motor_speed, -self.motor_speed, self.motor_speed, self.motor_speed], # FORWARD
+            [self.motor_speed, self.motor_speed, -self.motor_speed, -self.motor_speed], # BACKWARD
+            [self.motor_speed, self.motor_speed, self.motor_speed, self.motor_speed], # TURN_CW
+            [-self.motor_speed, -self.motor_speed, -self.motor_speed, -self.motor_speed], # TURN_CCW
+            [self.motor_speed*2, -self.motor_speed*2, -self.motor_speed*2, self.motor_speed*2], # LEFT
+            [-self.motor_speed*2, self.motor_speed*2, self.motor_speed*2, -self.motor_speed*2] # RIGHT
+        ]
+
+        # MOTOR INIT
         # MOTOR NUMBERING
         # 2 1
         # 3 0
+        i2c = busio.I2C(SCL, SDA)
+        self.pca = PCA9685(i2c, address=0x40)
+        self.pca.frequency = 100
+        duty_channels = [0, 3, 6, 9]
+        for i in duty_channels:
+            self.pca.channels[i].duty_cycle = 0xFFFF
         motor_channels = [[1, 2], [4, 5], [7, 8], [10, 11]]
-        self.motor = []
+        self.motor: List[motor.DCMotor] = []
         self.MOTOR_COUNT = 4
-        self.MOTOR_SAFE_DELAY = 0.01
         for a, b in motor_channels:
-            self.motor.append(motor.DCMotor(pca.channels[a], pca.channels[b]))
+            self.motor.append(motor.DCMotor(self.pca.channels[a], self.pca.channels[b]))
         for i in range(4):
             self.motor[i].decay_mode = motor.SLOW_DECAY
 
-    def stop(self):
+    def set_motor(self, robot_direction, speed_multiplier=1):
         time.sleep(self.MOTOR_SAFE_DELAY)
         for i in range(self.MOTOR_COUNT):
-            self.motor[i].throttle = 0
+            self.motor[i].throttle = self.speeds[robot_direction][i] * speed_multiplier
 
     def exit(self):
-        time.sleep(self.MOTOR_SAFE_DELAY)
-        for i in range(self.MOTOR_COUNT):
-            self.motor[i].throttle = 0
-        pca.deinit()
-
-    def forward(self):
-        time.sleep(self.MOTOR_SAFE_DELAY)
-        self.motor[0].throttle = -self.motor_speed
-        self.motor[1].throttle = -self.motor_speed
-        self.motor[2].throttle = self.motor_speed
-        self.motor[3].throttle = self.motor_speed
-
-    def backward(self):
-        time.sleep(self.MOTOR_SAFE_DELAY)
-        self.motor[0].throttle = self.motor_speed
-        self.motor[1].throttle = self.motor_speed
-        self.motor[2].throttle = -self.motor_speed
-        self.motor[3].throttle = -self.motor_speed
-
-    def turn_cw(self):
-        time.sleep(self.MOTOR_SAFE_DELAY)
-        for i in range(self.MOTOR_COUNT):
-            self.motor[i].throttle = self.motor_speed
-        # time.sleep(delay)
-        # self.stop()
-
-    def turn_ccw(self):
-        time.sleep(self.MOTOR_SAFE_DELAY)
-        for i in range(self.MOTOR_COUNT):
-            self.motor[i].throttle = -self.motor_speed
-        # time.sleep(delay)
-        # self.stop()
-
-    def left(self):
-        time.sleep(self.MOTOR_SAFE_DELAY)
-        self.motor[0].throttle = self.motor_speed*2
-        self.motor[1].throttle = -self.motor_speed*2
-        self.motor[2].throttle = -self.motor_speed*2
-        self.motor[3].throttle = self.motor_speed*2
-
-    def right(self):
-        time.sleep(self.MOTOR_SAFE_DELAY)
-        self.motor[0].throttle = -self.motor_speed*2
-        self.motor[1].throttle = self.motor_speed*2
-        self.motor[2].throttle = self.motor_speed*2
-        self.motor[3].throttle = -self.motor_speed*2
-
-
-if __name__ == '__main__':
-    drive = Drive()
-    print("Press f, b, cw, ccw, l, r to go, q : stop, x : exit")
-    while True:
-        key = input("Where : ")
-        if key == 'f':
-            drive.forward()
-        elif key == 'b':
-            drive.backward()
-        elif key == 'cw':
-            drive.turn_cw()
-        elif key == 'ccw':
-            drive.turn_ccw()
-        elif key == 'q':
-            drive.stop()
-        elif key == 'x':
-            drive.exit()
-            break
+        self.set_motor(self.STOP)
+        self.pca.deinit()
