2a449a4ba015234b92ff3489e55a652c5425d562
inukara <17498820+inukara@users.noreply.github.com>
2023-11-17 15:58:52 +0900

    drive_map: drive facing front

--- drive_map.py
+++ drive_map.py
@@ -9,6 +9,11 @@ class DriveMap:
         self.y = 0
         self.drive = drive.Drive()
         self.done = False
+        self.direction = 0
+        self.UP = 0
+        self.DOWN = 1
+        self.LEFT = 2
+        self.RIGHT = 3
 
     def start(self):
         for i in range(len(self.map)):
@@ -21,10 +26,8 @@ class DriveMap:
         thr.start()
     
     def drive_map(self):
-        i = 0
+        print(self.map)
         while not self.done:
-            print(i)
-            i = i + 1
             print(self.x, self.y)
             self.next(1)
             time.sleep(0.3)
@@ -35,28 +38,52 @@ class DriveMap:
             try:
                 if self.map[self.x-1][self.y] == 9:
                     self.x -= 1
-                    self.drive.set_motor('forward', 0.1, 0, False)
+                    if self.direction != self.UP:
+                        if self.direction == self.LEFT:
+                            self.drive.set_motor('turn_cw', 1)
+                        elif self.direction == self.RIGHT:
+                            self.drive.set_motor('turn_ccw', 1)
+                        self.direction = self.UP
+                    self.drive.set_motor('forward', 0, False)
                     continue
             except IndexError:
                 pass
             try:
                 if self.map[self.x+1][self.y] == 9:
                     self.x += 1
-                    self.drive.set_motor('backward', 0.1, 0, False)
+                    if self.direction != self.DOWN:
+                        if self.direction == self.LEFT:
+                            self.drive.set_motor('turn_ccw', 1)
+                        elif self.direction == self.RIGHT:
+                            self.drive.set_motor('turn_cw', 1)
+                        self.direction = self.DOWN
+                    self.drive.set_motor('forward', 0, False)
                     continue
             except IndexError:
                 pass
             try:
                 if self.map[self.x][self.y-1] == 9:
                     self.y -= 1
-                    self.drive.set_motor('left', 0.1, 0, False)
+                    if self.direction != self.LEFT:
+                        if self.direction == self.UP:
+                            self.drive.set_motor('turn_ccw', 1)
+                        elif self.direction == self.DOWN:
+                            self.drive.set_motor('turn_cw', 1)
+                        self.direction = self.LEFT
+                    self.drive.set_motor('forward', 0, False)
                     continue
             except IndexError:
                 pass
             try:
                 if self.map[self.x][self.y+1] == 9:
                     self.y += 1
-                    self.drive.set_motor('right', 0.1, 0, False)
+                    if self.direction != self.RIGHT:
+                        if self.direction == self.UP:
+                            self.drive.set_motor('turn_cw', 1)
+                        elif self.direction == self.DOWN:
+                            self.drive.set_motor('turn_ccw', 1)
+                        self.direction = self.RIGHT
+                    self.drive.set_motor('forward', 0, False)
                     continue
             except IndexError:
                 pass
